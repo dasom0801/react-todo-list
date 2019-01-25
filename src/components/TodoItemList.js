2bb9d1cd80dc0e1350a0cf86ce60d2c6ff663e82
Dasom Kim <dev.dasom1012@gmail.com>
2019-01-25 15:40:42 +0900

    react todo list 만들기

---
+++ src/components/TodoItemList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+class TodoItemList extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    // 컴포넌트 리렌더링 여부를 결정
+    // 업데이트에 영향을 끼치는 조건을 return해주면 된다. 
+    // 구현하지 않으면 기본 true
+    return this.props.todos !== nextProps.todos;
+  }
+
+  render() {
+    const { todos, onToggle, onRemove } = this.props;
+
+    const todoList = todos.map(({id, text, checked}) => (
+        <TodoItem 
+          id={id}
+          text={text}
+          checked={checked}
+          onToggle={onToggle}
+          onRemove={onRemove}
+          key={id}
+        />
+      ) 
+    );
+
+    return (
+      <div>
+        {todoList}
+      </div>
+    );
+ }
+}
+
+export default TodoItemList;
